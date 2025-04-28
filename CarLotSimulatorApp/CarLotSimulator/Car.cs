06041d641e9b3670411ed2694bdce6fb03df3de7
Peter Kalinowski <peterkalinowski@Peters-Mac.lan>
2025-04-28 18:59:52 -0400

    updated for staticfield and incremented for every new car

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -4,13 +4,27 @@ namespace CarLotSimulator
 {
     public class Car
     {
+        public Car()
+        {
+            CarLot.numberOfCars++;
+            //classname.staticmember
+        }
+
+        public Car(string make, string model, int year, bool isDriveable)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            IsDriveable = isDriveable;
+            CarLot.numberOfCars++;
+        }
+
         public int Year { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public string EngineNoise { get; set; }
         public string HonkNoise { get; set; }
         public bool IsDriveable { get; set; }
-
 
         public void MakeEngineNoise(string engineNoise)
         {
@@ -23,14 +37,5 @@ namespace CarLotSimulator
             HonkNoise = honkNoise;
             Console.WriteLine($"The {Make} {Model} goes {HonkNoise}");
         }
-        
-        public Car() { }
-        public Car(string make, string model, int year, bool isDriveable)
-        {
-            Make = make;
-            Model = model;
-            Year = year;
-            IsDriveable = isDriveable;
-        }
     }
-}   
+}
